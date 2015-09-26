1aba041c7ac5532b536763a76bbae5046a9eebc0
Victor Atteh <victoratteh@gmail.com>
2015-09-26 17:10:42 -0400

    add cherrio

---
+++ server/app/routes/artistInfluences/index.js
@@ -0,0 +1,23 @@
+'use strict';
+var router = require('express').Router();
+var mongoose = require('mongoose');
+var request = require('request');
+var cheerio = require('cheerio');
+
+router.get('/:artistName', function(req, res, next) {
+	
+	var url = 'http://www.mtv.com/artists/' + req.params.artistName.replace(/ /g,"-") + '/related-artists/?filter=influencedBy';
+
+  request(url, function(err, resp, body){
+    var influences = [];
+    var $ = cheerio.load(body);
+      var divs = $('div.title.multiline');
+    	$(divs).each(function(i, elem){
+        console.log(elem.children[0].data.replace(/\r\n\t/g,"-"));
+      	influences.push(elem.innerHTML);
+    	});
+	  res.json(influences);
+  });
+});
+
+module.exports = router;
