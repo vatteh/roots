dfbab9cfe253cddb12ccc77e3cc1f02dce1e7140
Alex Liu <yuningalexliu@gmail.com>
2015-09-27 13:17:06 -0400

    mvp

--- browser/js/discover/discover.js
+++ browser/js/discover/discover.js
@@ -2,13 +2,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-1', {
         url: '/discover-1',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 1;
     		$scope.pageClass = 'discover-1';
 
         //Get Artist Info
     		SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
             $scope.transitionTo = function() {
@@ -27,13 +28,15 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-2', {
         url: '/discover-2',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 2;
     		$scope.pageClass = 'discover-2';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
+
         });
 
     		$scope.transitionTo = function() {
@@ -53,13 +56,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-3', {
         url: '/discover-3',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 3;
     		$scope.pageClass = 'discover-3';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
     		$scope.transitionTo = function() {
@@ -79,13 +83,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-4', {
         url: '/discover-4',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 4;
     		$scope.pageClass = 'discover-4';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
     		$scope.transitionTo = function() {
@@ -105,13 +110,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-5', {
         url: '/discover-5',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 5;
     		$scope.pageClass = 'discover-5';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
     		$scope.transitionTo = function() {
@@ -131,9 +137,16 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-6', {
         url: '/discover-6',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 6;
     		$scope.pageClass = 'discover-6';
+
+        //Get Artist Info
+        SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
+          $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
+        });
+
     		$scope.transitionTo = function() {
                 ArtistInfluences.getArtistInfluences($rootScope.currArtist).then(function(artist) {
                    $rootScope.currArtist = artist;
@@ -151,13 +164,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-7', {
         url: '/discover-7',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 7;
     		$scope.pageClass = 'discover-7';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
     		$scope.transitionTo = function() {
@@ -177,13 +191,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-8', {
         url: '/discover-8',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 8;
     		$scope.pageClass = 'discover-8';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
     		$scope.transitionTo = function() {
@@ -203,13 +218,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-9', {
         url: '/discover-9',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 9;
     		$scope.pageClass = 'discover-9';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
     		$scope.transitionTo = function() {
@@ -229,13 +245,14 @@ app.config(function ($stateProvider) {
     $stateProvider.state('discover-10', {
         url: '/discover-10',
         templateUrl: 'js/discover/discover.html',
-        controller: function($rootScope, $scope, $state, ArtistInfluences) {
+        controller: function($rootScope, $scope, $state, ArtistInfluences, SpotifyInfo, $sce) {
     		$scope.pageNumber = 10;
     		$scope.pageClass = 'discover-10';
 
         //Get Artist Info
         SpotifyInfo.searchForArtist($rootScope.currArtist).then(function(data) {
           $scope.artistData = data
+          $scope.recording = $sce.trustAsResourceUrl($scope.artistData.artistFirstTopTrack.preview_url);
         });
 
     		$scope.transitionTo = function() {
