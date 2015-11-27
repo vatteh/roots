fd97af599a355b8844583d316b0ab1d1aa3b7fb9
Victor Atteh <victoratteh@gmail.com>
2015-11-27 16:10:22 -0500

    delete fsg files and fix spacing

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,59 +1,59 @@
 app.config(function($stateProvider) {
-  $stateProvider.state('home', {
-    url: '/',
-    templateUrl: 'js/home/home.html',
-    controller: 'HomeCtrl'
-  });
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl'
+    });
 });
 
 app.controller('HomeCtrl', function($rootScope, $scope, $state, $sce, CurrentTopArtists, ArtistInfluences, SpotifyInfo) {
-  $rootScope.discoverPage = 0;
+    $rootScope.discoverPage = 0;
 
-  CurrentTopArtists.getCurrentTopArtists()
-    .then(function(response) {
-      $scope.currentTopArtists = response;
-    }).catch(function(err) {
-      throw new Error('Colud not get current top spotify artists!');
-      console.log(err);
-    });
+    CurrentTopArtists.getCurrentTopArtists()
+        .then(function(response) {
+            $scope.currentTopArtists = response;
+        }).catch(function(err) {
+            throw new Error('Colud not get current top spotify artists!');
+            console.log(err);
+        });
 
-  $scope.getMoreArtists = function() {
-    return CurrentTopArtists.getCurrentTopArtists()
-      .then(function(response) {
-        $scope.currentTopArtists = $scope.currentTopArtists.concat(response);
-        return;
-      }).catch(function(err) {
-        throw new Error('Colud not get more spotify artists!');
-        console.log(err);
-      });
-  };
+    $scope.getMoreArtists = function() {
+        return CurrentTopArtists.getCurrentTopArtists()
+            .then(function(response) {
+                $scope.currentTopArtists = $scope.currentTopArtists.concat(response);
+                return;
+            }).catch(function(err) {
+                throw new Error('Colud not get more spotify artists!');
+                console.log(err);
+            });
+    };
 
-  $rootScope.nextInfluencer = function(artistName) {
-    var newCurrentArtist;
+    $rootScope.nextInfluencer = function(artistName) {
+        var newCurrentArtist;
 
-    // ArtistInfluences.getArtistInfluences will get all influences that will be seen for the artist selected in the front page.
-    ArtistInfluences.getArtistInfluences(artistName)
-      .then(function(artist) {
-        if (artist.name !== 'StatusCodeError' && $rootScope.discoverPage < 10) {
-          console.log("Got influencer for " + artistName + ": " + artist.name);
-          $rootScope.discoverPage++;
-          newCurrentArtist = artist.name;
-          return SpotifyInfo.searchForArtist(newCurrentArtist);
-        } else {
-          throw new Error('No artist influencer found for - ' + artistName);
-        }
-      }).then(function(data) {
-        if (data !== null) {
-          $rootScope.artistData = data;
-          $rootScope.recording = $sce.trustAsResourceUrl($rootScope.artistData.artistFirstTopTrack.preview_url);
-          $rootScope.currArtist = newCurrentArtist;
-          $state.go('discover-' + $rootScope.discoverPage);
-        } else {
-          throw new Error('No spotify info found for influencer - ' + $rootScope.currArtist);
-        }
-      }).catch(function(err) {
-        $rootScope.discoverPage = 0;
-        $state.go('home');
-      });
-  };
+        // ArtistInfluences.getArtistInfluences will get all influences that will be seen for the artist selected in the front page.
+        ArtistInfluences.getArtistInfluences(artistName)
+            .then(function(artist) {
+                if (artist.name !== 'StatusCodeError' && $rootScope.discoverPage < 10) {
+                    console.log("Got influencer for " + artistName + ": " + artist.name);
+                    $rootScope.discoverPage++;
+                    newCurrentArtist = artist.name;
+                    return SpotifyInfo.searchForArtist(newCurrentArtist);
+                } else {
+                    throw new Error('No artist influencer found for - ' + artistName);
+                }
+            }).then(function(data) {
+                if (data !== null) {
+                    $rootScope.artistData = data;
+                    $rootScope.recording = $sce.trustAsResourceUrl($rootScope.artistData.artistFirstTopTrack.preview_url);
+                    $rootScope.currArtist = newCurrentArtist;
+                    $state.go('discover-' + $rootScope.discoverPage);
+                } else {
+                    throw new Error('No spotify info found for influencer - ' + $rootScope.currArtist);
+                }
+            }).catch(function(err) {
+                $rootScope.discoverPage = 0;
+                $state.go('home');
+            });
+    };
 });
