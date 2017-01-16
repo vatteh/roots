41305ebef51b7661816fbc28a22de537cdf62e5a
vatteh <victor@artivest.co>
2017-01-16 01:00:31 -0500

    make hide artists animation

--- browser/js/home/HomeController.js
+++ browser/js/home/HomeController.js
@@ -3,15 +3,23 @@
 app.controller('HomeController', function($scope, $state, $q, CurrentTopArtistsFactory, ArtistInfluencesFactory, SpotifyInfoFactory, currentTopArtists) {
     this.displayedArtists = currentTopArtists;
 
-    this.startDiscovery = artistName => {
+    // should probally find a smaller image to use here
+    // this.displayedArtists.forEach((artist) => {
+    //     artist.displayImage = artist.images[Math.floor(Math.random() * artist.images.length)];
+    // });
+
+    this.startDiscovery = artist => {
+        this.artistChosen = true;
+        artist.selected = true;
         let influencer;
-        return ArtistInfluencesFactory.getArtistInfluences(artistName).then(artist => {
+
+        return ArtistInfluencesFactory.getArtistInfluences(artist.name).then(artist => {
             if (artist.name !== 'StatusCodeError') {
                 influencer = artist.name;
-                console.log("Got influencer for " + artistName + ": " + influencer);
+                console.log("Got influencer for " + artist.name + ": " + influencer);
                 return SpotifyInfoFactory.searchForArtist(influencer);
             } else {
-                throw new Error('No artist influencer found for - ' + artistName);
+                throw new Error('No artist influencer found for - ' + artist.name);
             }
         }).then(data => {
             if (data !== null) {
