e14016bc5b07ea5dd6b82642075c79baa9706d81
vatteh <victor@artivest.co>
2017-02-09 00:54:14 -0500

    more styling

--- browser/js/common/components/sampleTracks.js
+++ browser/js/common/components/sampleTracks.js
@@ -7,16 +7,19 @@ app.component('sampleTracks', {
     },
     template: `
         <div layout='row'>
-            <div ng-repeat='track in $ctrl.tracks track by $index' flex='33' layout='row' layout-align='space-between center' ng-click='$ctrl.playTrack(track, $index)'>
-                <img flex='initial' ng-src='{{track.image.url}}' class='materal-padding'/>
-                <div flex>
-                    <div class='md-body-2 truncate' ng-bind='track.name'></div>
-                    <div class='md-caption truncate' ng-bind='track.albumName'></div>
-                </div>
-                <div flex='initial' class='materal-padding'>
-                    <i class='fa fa-lg' ng-class="track.isPlaying ? 'fa-play' : 'fa-pause'"></i>
-                </div>
-            </div>
+            <md-list ng-cloak style='width: 100%;'>
+                <md-subheader class='md-no-sticky'>Sample Tracks</md-subheader>
+                <md-list-item ng-repeat='track in $ctrl.tracks track by $index' layout='row' layout-align='space-between center' ng-click='$ctrl.playTrack(track, $index)'>
+                    <img flex='none' ng-src='{{track.image.url}}' class='sample-tracks__image materal-padding'/>
+                    <div flex='auto' class='sample-tracks__text'>
+                        <div class='md-body-2 truncate' ng-bind='track.name'></div>
+                        <div class='md-caption truncate' ng-bind='track.albumName'></div>
+                    </div>
+                    <div flex='none' class='sample-tracks__play-icon materal-padding'>
+                        <i class='fa fa-lg' ng-class="track.isPlaying ? 'fa-play' : 'fa-pause'"></i>
+                    </div>
+                </md-list-item>
+            </md-list>
             <iframe id='iframe' width='0' height='0' class='visibility: none' frameborder='0'>
         </div>`,
     controller: function($sce) {
