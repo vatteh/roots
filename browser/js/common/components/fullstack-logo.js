7144ec5bdacf962647a36d910644ee77ced97f0b
vatteh <victor@artivest.co>
2017-03-10 00:26:23 -0500

    remove fullstack-logo directive and favicon

--- browser/js/common/components/fullstack-logo.js
+++
@@ -1,8 +0,0 @@
-/* jshint esversion:6 */
-
-app.directive('fullstackLogo', () => {
-    return {
-        restrict: 'E',
-        template: `<img src="https://jlau-bucket-1.s3.amazonaws.com/uploads/topic/image/42/fullstack.png"/>`
-    };
-});
