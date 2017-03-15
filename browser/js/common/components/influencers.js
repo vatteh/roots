d9a9ab5f5f2a9c16487f9ec46b33c78faf10f3c0
vatteh <victor@artivest.co>
2017-03-15 01:20:14 -0400

    add credit text

--- browser/js/common/components/influencers.js
+++ browser/js/common/components/influencers.js
@@ -30,7 +30,7 @@ app.component('influnecers', {
         };
 
         this.getInfluencerLevelBorder = artist => {
-            return "5px solid " + this.influencerLevelColors[artist.weight - 1];
+            return '5px solid ' + this.influencerLevelColors[artist.weight - 1];
         };
         
         this.influencerLevelColors = ['#E8EAF6', '#C5CAE9', '#9FA8DA', '#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', '#303F9F', '#283593', '#1A237E'];
