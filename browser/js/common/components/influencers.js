6d5d3c403d20ba03689d4410607e09ad54eb6b10
vatteh <victor@artivest.co>
2017-03-15 01:06:30 -0400

    add influencer level to tooltip and border

--- browser/js/common/components/influencers.js
+++ browser/js/common/components/influencers.js
@@ -8,8 +8,8 @@ app.component('influnecers', {
     template: `
         <div class='artist-choices-container' layout='row' layout-align='center center'>
             <div ng-repeat='artist in $ctrl.artists track by $index' class='artist-container' ng-class='{"hide-artist": $ctrl.artistChosen && !artist.selected}'>
-                <md-tooltip md-direction='top' ng-bind='artist.name'></md-tooltip>
-                <img class='artist-image' ng-class='{"highlight-selected-artist": artist.selected}' ng-click='$ctrl.selectArtist(artist, $index)' ng-src='{{artist.spotifyThumbnail.url}}' />
+                <md-tooltip md-direction='top' ng-bind='artist.weight ? (artist.name + " - Influencer Level: " + artist.weight) : artist.name'></md-tooltip>
+                <img class='artist-image' ng-class='{"highlight-selected-artist": artist.selected}' ng-click='$ctrl.selectArtist(artist, $index)' ng-src='{{artist.spotifyThumbnail.url}}' ng-style='{"border": $ctrl.getInfluencerLevelBorder(artist)}'/>
             </div>
             <p ng-if='$ctrl.artists.length === 0'>No influencers found for this artist.</p>
         </div>`,
@@ -28,5 +28,11 @@ app.component('influnecers', {
                 $state.go('discover', { artistThumbnailInfo: selectedArtist });
             });
         };
+
+        this.getInfluencerLevelBorder = artist => {
+            return "5px solid " + this.influencerLevelColors[artist.weight - 1];
+        };
+        
+        this.influencerLevelColors = ['#E8EAF6', '#C5CAE9', '#9FA8DA', '#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', '#303F9F', '#283593', '#1A237E'];
     }
 });
