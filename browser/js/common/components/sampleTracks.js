385921499b6239ad7da40e9ef1c5b26ea986f5de
vatteh <victor@artivest.co>
2017-03-20 00:15:38 -0400

    remove meta tag again :(

--- browser/js/common/components/sampleTracks.js
+++ browser/js/common/components/sampleTracks.js
@@ -6,26 +6,24 @@ app.component('sampleTracks', {
         tracks: '<'
     },
     template: `
-        <div layout='row'>
-            <md-list ng-cloak style='width: 100%;'>
-                <div layout='row' class='sample-tracks__container'>
-                    <h4 flex class='sample-tracks__title md-title md-no-sticky'>Sample Tracks</h4>
-                    <md-switch flex='nogrow' class='sample-tracks__autoplay-switch' ng-model='$ctrl.StateService.autoPlayState'>Autoplay</md-switch>
+        <md-list ng-cloak>
+            <div layout='row' class='sample-tracks__container'>
+                <h4 flex class='sample-tracks__title md-title md-no-sticky'>Sample Tracks</h4>
+                <md-switch flex='nogrow' class='sample-tracks__autoplay-switch' ng-model='$ctrl.StateService.autoPlayState'>Autoplay</md-switch>
+            </div>
+            <md-list-item ng-repeat='track in $ctrl.tracks track by $index' layout='row' layout-align='space-between center' ng-click='$ctrl.playTrack(track, $index)' aria-label='Sample Track'>
+                <img flex='none' ng-src='{{track.image.url}}' class='sample-tracks__image materal-padding'/>
+                <div flex='auto' class='sample-tracks__text'>
+                    <div class='md-body-2 truncate' ng-bind='track.name'></div>
+                    <div class='md-caption truncate' ng-bind='track.albumName'></div>
                 </div>
-                <md-list-item ng-repeat='track in $ctrl.tracks track by $index' layout='row' layout-align='space-between center' ng-click='$ctrl.playTrack(track, $index)' aria-label='Sample Track'>
-                    <img flex='none' ng-src='{{track.image.url}}' class='sample-tracks__image materal-padding'/>
-                    <div flex='auto' class='sample-tracks__text'>
-                        <div class='md-body-2 truncate' ng-bind='track.name'></div>
-                        <div class='md-caption truncate' ng-bind='track.albumName'></div>
-                    </div>
-                    <div flex='none' ng-click='$ctrl.playTrack(track, $index)' class='sample-tracks__play-icon materal-padding'>
-                        <i class='fa fa-lg' ng-class="track.isPlaying ? 'fa-play' : 'fa-pause'"></i>
-                        <md-progress-circular md-mode='determinate' value='{{track.progressValue}}' md-diameter='64' class='fade' ng-show='track.isPlaying'></md-progress-circular>
-                    </div>
-                </md-list-item>
-            </md-list>
-            <audio id='audioTag'></audio>
-        </div>`,
+                <div flex='none' ng-click='$ctrl.playTrack(track, $index)' class='sample-tracks__play-icon materal-padding'>
+                    <i class='fa fa-lg' ng-class="track.isPlaying ? 'fa-play' : 'fa-pause'"></i>
+                    <md-progress-circular md-mode='determinate' value='{{track.progressValue}}' md-diameter='64' class='fade' ng-show='track.isPlaying'></md-progress-circular>
+                </div>
+            </md-list-item>
+        </md-list>
+        <audio id='audioTag'></audio>`,
     controller: function($scope, $sce, $interval, StateService) {
         this.StateService = StateService;
         let audioElement = document.getElementById('audioTag');
